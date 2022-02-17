dea13550472faf015ad41c53c1fbe08db763d1f6
spranavr <spranavr@SPRANAVR-LAP.idc.oracle.com>
2022-02-18 00:42:36 +0530

    hosting files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent,pathMatch:'full'},
-  {path:'tenflix',component:HomeComponent,pathMatch:'full'},
-  {path:'tenflix/docs',component:HomeComponent,pathMatch:'full'},
-  {path:'home',component:HomeComponent,pathMatch:'full'}
+  {path:'',component:HomeComponent,pathMatch:'full'}
 ];
 
 @NgModule({
