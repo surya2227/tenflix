c592f60770b437ec04d6c86bf399c6c156ab038d
spranavr <spranavr@SPRANAVR-LAP.idc.oracle.com>
2022-02-18 00:19:23 +0530

    build files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'tenflix',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent,pathMatch:'full'}
 ];
 
